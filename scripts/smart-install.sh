fc408abbed71f22bb3624e8c10cf6999c1083921
ajay <ajaysidhu17@gmail.com>
2020-09-30 12:25:59 +0530

    updated smart script

--- scripts/smart-install.sh
+++ scripts/smart-install.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-changedFiles="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
 
-checkForChangedFiles() {
-	echo "$changedFiles" | grep --quiet "$1" && eval "$2"
+check_run() {
+	echo "$changed_files" | grep --quiet "$1" && eval "$2"
 }
 
-packageJsonHasChanged() {
-  echo "Changes to package-lock.json detected, installing updates"
-  npm i
-}
-
-checkForChangedFiles package-lock.json packageJsonHasChanged
+check_run package-lock.json "npm install"
